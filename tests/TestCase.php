fc2b2911dac3e867e1eddc07932817b9f23b4fa9
Brian Matovu <mtvbrianking@gmail.com>
2019-03-30 17:47:33 +0300

    Fix tests in phpunit 8

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,7 @@ abstract class TestCase extends Orchestra
     /**
      * {@inheritdoc}
      */
-    public function setUp()
+    public function setUp() : void
     {
         parent::setUp();
 
