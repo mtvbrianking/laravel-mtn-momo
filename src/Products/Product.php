f7ef0cf8e82ca26af207a2eb21e73ea134554ec5
bmatovu <mtvbrianking@gmail.com>
2021-02-27 09:58:20 +0300

    Remove phpdoc throws from abstract method.

--- src/Products/Product.php
+++ src/Products/Product.php
@@ -339,8 +339,6 @@ abstract class Product
     /**
      * Request access token.
      *
-     * @throws \GuzzleHttp\Exception\RequestException;
-     *
      * @return array
      */
     abstract public function getToken();
